6ff74e0ee6898dc15ab63f24eecdc4d9ee4870df
Daniel Greene <d.b.greene101@gmail.com>
2017-07-10 20:47:26 -0500

    not finished

---
+++ for-loops.js
@@ -0,0 +1,13 @@
+var j = 5;
+for (i = 0; j < 120; i++){
+    j = j + 10;
+    console.log(j);
+}
+
+var d = 4096;
+for (i = 0; d > 1; i++){
+    d = d / 2;
+    console.log(d);
+}
+
+var r = ("President #" +var num+ "was" var p);
